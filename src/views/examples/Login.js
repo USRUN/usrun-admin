54547b03dc1eff32ec00e74768f878a4da7b0cec
HuyNa <anhhuy980413@gmail.com>
2020-07-25 17:55:33 +0700

    first commit

---
+++ src/views/examples/Login.js
@@ -0,0 +1,150 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Row,
+  Col,
+  CardFooter
+} from "reactstrap";
+
+class Login extends React.Component {
+  render() {
+    return (
+      <>
+        <Col lg="5" md="7">
+          <Card className="bg-secondary shadow border-0">
+            <CardBody className="px-lg-5 pt-lg-5">
+              <div className="text-center text-muted mb-4">
+                <h2>Đăng nhập tài khoản</h2>
+              </div>
+              <Form role="form">
+                <FormGroup className="mb-3">
+                  <InputGroup className="input-group-alternative">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="ni ni-email-83" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input placeholder="Tài khoản" type="email" autoComplete="new-email"/>
+                  </InputGroup>
+                </FormGroup>
+                <FormGroup>
+                  <InputGroup className="input-group-alternative">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="ni ni-lock-circle-open" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input placeholder="Mật khẩu" type="password" autoComplete="new-password"/>
+                  </InputGroup>
+                </FormGroup>
+                <div className="custom-control custom-control-alternative custom-checkbox">
+                  <input
+                    className="custom-control-input"
+                    id=" customCheckLogin"
+                    type="checkbox"
+                  />
+                  <label
+                    className="custom-control-label"
+                    htmlFor=" customCheckLogin"
+                  >
+                    <span className="text-muted">Ghi nhớ đăng nhập</span>
+                  </label>
+                </div>
+                <div className="text-center">
+                  <Button className="mt-4" color="primary" type="button">
+                  ĐĂNG NHẬP
+                  </Button>
+                </div>
+              </Form>
+            </CardBody>
+            <CardFooter>
+            <div className="text-muted text-center mt-2 mb-3">
+                <small>Đăng nhập với</small>
+              </div>
+              <div className="btn-wrapper text-center">
+                <Button
+                  className="btn-neutral btn-icon"
+                  color="default"
+                  href="#pablo"
+                  onClick={e => e.preventDefault()}
+                >
+                  <span className="btn-inner--icon">
+                    <img
+                      alt="..."
+                      src={require("assets/img/icons/common/github.svg")}
+                    />
+                  </span>
+                  <span className="btn-inner--text">Github</span>
+                </Button>
+                <Button
+                  className="btn-neutral btn-icon"
+                  color="default"
+                  href="#pablo"
+                  onClick={e => e.preventDefault()}
+                >
+                  <span className="btn-inner--icon">
+                    <img
+                      alt="..."
+                      src={require("assets/img/icons/common/google.svg")}
+                    />
+                  </span>
+                  <span className="btn-inner--text">Google</span>
+                </Button>
+              </div>
+            </CardFooter>
+          </Card>
+          <Row className="mt-3">
+            <Col xs="6">
+              <a
+                className="text-light"
+                href="#pablo"
+                onClick={e => e.preventDefault()}
+              >
+                <small style={{color:'#fff'}}>Quên mật khẩu?</small>
+              </a>
+            </Col>
+            <Col className="text-right" xs="6">
+              <a
+                className="text-light"
+                href="#pablo"
+                onClick={e => e.preventDefault()}
+              >
+                <small style={{color:'#fff'}}>Tạo tài khoản</small>
+              </a>
+            </Col>
+          </Row>
+        </Col>
+      </>
+    );
+  }
+}
+
+export default Login;
